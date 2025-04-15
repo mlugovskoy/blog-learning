d314c243c4e3e2d98a9669de8f40f038cce46aad
mlugovskoy <lugovskoy19@mail.ru>
2025-04-15 19:43:06 +0900

    feat: Add models and migrations

---
+++ database/migrations/2025_04_14_151536_create_comments_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
+            $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
+            $table->longText('body');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
