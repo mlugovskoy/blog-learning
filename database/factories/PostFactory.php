16cdbc0a13ab4c620bbd2f938a2baa3405554ff1
mlugovskoy <lugovskoy19@mail.ru>
2025-05-29 22:10:48 +0900

    feat: Change post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,13 +5,17 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use SplFileInfo;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
+    private static Collection $fixtures;
+
     public function definition(): array
     {
         $title = str(fake()->sentence())->beforeLast('.')->title();
@@ -22,5 +26,23 @@ class PostFactory extends Factory
             'slug' => Str::slug($title),
             'body' => Collection::times(4, fn() => fake()->realText(1250))->join(PHP_EOL . PHP_EOL)
         ];
+    }
+
+    public function withFixture()
+    {
+        $posts = static::getFixtures()
+            ->map(fn(string $contents) => str($contents)->explode("\n", 2))
+            ->map(fn(Collection $parts) => [
+                'title' => str($parts[0])->trim()->after('# '),
+                'body' => str($parts[1])->trim()
+            ]);
+
+        return $this->sequence(...$posts);
+    }
+
+    private function getFixtures(): Collection
+    {
+        return self::$fixtures ??= collect(File::files(database_path('factories/articles')))
+            ->map(fn(SplFileInfo $fileInfo) => $fileInfo->getContents());
     }
 }
